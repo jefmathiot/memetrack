fe2e23c261efff9db55add2609b735f5e50716fa
Jef Mathiot <jeff.mathiot@gmail.com>
2015-04-07 12:32:25 +0200

    Seed with a few images

--- memetrack/db/seeds.rb
+++ memetrack/db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Dir.glob(File.join(File.dirname(__FILE__), 'memes', '*.*')).each do |file|
+  Meme.create!(
+    picture: File.open(file),
+    tags: File.basename(file, '.*').split('-')
+  )
+end
