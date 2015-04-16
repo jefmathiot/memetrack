f338a8c3ecba57689ae6b2d305c09beb8e41cbae
Jef Mathiot <jeff.mathiot@gmail.com>
2015-04-16 09:42:27 +0200

    Do no seed in test env

--- memetrack/db/seeds.rb
+++ memetrack/db/seeds.rb
@@ -1,6 +1,9 @@
-Dir.glob(File.join(File.dirname(__FILE__), 'memes', '*.*')).each do |file|
-  Meme.create!(
-    picture: File.open(file),
-    tags: File.basename(file, '.*').split('-')
-  )
+unless Rails.env.test?
+  Dir.glob(File.join(File.dirname(__FILE__), 'memes', '*.*')).each do |file|
+    puts "Seeding #{file}"
+    Meme.create!(
+      picture: File.open(file),
+      tags: File.basename(file, '.*').split('-')
+    )
+  end
 end
